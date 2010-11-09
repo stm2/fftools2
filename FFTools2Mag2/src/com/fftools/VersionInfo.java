16ee912e7e6f0957f723994be45b7e4bc6345119
eressea <eressea@gdr-group.com>
2010-11-09 16:42:56 +0000

    0.43 Bugfix Trankeffekt, Feature "other"

--- FFTools2Mag2/src/com/fftools/VersionInfo.java
+++ FFTools2Mag2/src/com/fftools/VersionInfo.java
@@ -19,7 +19,7 @@ public class VersionInfo {
 	private static final OutTextClass outText = OutTextClass.getInstance();
 	
 	
-	private static final String versionInfo = "0.42";
+	private static final String versionInfo = "0.43";
 
 	private static String toAdd = "";
 	
